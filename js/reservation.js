ad808089e628a16269bb1af4206fc97524f26eef
Jerad D. Millay <jerad.millay@gmail.com>
2017-11-13 18:05:44 -0500

    added submit button build alert

---
+++ js/reservation.js
@@ -0,0 +1,23 @@
+
+/////datepicker//////
+
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
+
+
+/////Put 'er in stone! submission//////
+
+// function troll(selection) {
+//   if selection = '0'
+//   alert("You think you're funny don't ya? Saving a spot for zero people...try again troll");
+// };
+//
+
+
+
+/////Put 'er in stone! submission//////
+ // document.getElementByID.("#reserve").submit();
+ function reserve() {
+   alert("Yeeeeeaaaahhhhhhh, we're working on it...patience is virtue...or so they say");
+ };
